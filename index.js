982c5136a5ce4cf5100eca582ca707b8a41b3a50
Mary Karakida <makarakida@gmail.com>
2022-07-04 11:46:37 -0300

    chore: adjust code for deploy on heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import accountsRouters from './src/routes/accountsRouters.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
